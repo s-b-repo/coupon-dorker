a9e654fc4def4a4ce410b6ecaf50453615545367
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-12-23 10:17:15 +0200

    Update popkns.py

--- popkns.py
+++ popkns.py
@@ -14,21 +14,121 @@ def banner():
 
 def generate_dorks(filename="dorks.txt"):
     dorks = [
-        "site:example.com inurl:coupon",
-        "site:retailmenot.com inurl:discount",
-        "site:groupon.com filetype:pdf",
-        "intitle:\"coupon codes\" site:*.edu",
-        "intitle:\"promo codes\" site:*.gov",
-        "site:twitter.com inurl:\"coupon\"",
-        "site:pastebin.com \"coupon code\"",
-        "\"valid coupon code\" filetype:txt",
-        "\"discount codes\" filetype:csv",
-        "site:facebook.com inurl:\"coupon\"",
-        "site:reddit.com \"coupon code\"",
-        "\"active promo codes\" filetype:docx",
-        "site:instagram.com \"promo code\"",
-        "site:medium.com \"discount coupon\"",
-        "\"recently added coupons\" site:*.com"
+        
+    # General coupon dorks
+    "site:example.com inurl:coupon",
+    "site:retailmenot.com inurl:discount",
+    "site:groupon.com filetype:pdf",
+    "intitle:\"coupon codes\" site:*.edu",
+    "intitle:\"promo codes\" site:*.gov",
+    "site:twitter.com inurl:\"coupon\"",
+    "site:pastebin.com \"coupon code\"",
+    "\"valid coupon code\" filetype:txt",
+    "\"discount codes\" filetype:csv",
+    "site:facebook.com inurl:\"coupon\"",
+    "site:reddit.com \"coupon code\"",
+    "\"active promo codes\" filetype:docx",
+    "site:instagram.com \"promo code\"",
+    "site:medium.com \"discount coupon\"",
+    "\"recently added coupons\" site:*.com",
+
+    # File type-specific searches
+    "\"coupon code\" filetype:xls",
+    "\"promo code\" filetype:doc",
+    "\"discount codes\" filetype:json",
+    "\"valid coupons\" filetype:yaml",
+    "\"discounts list\" filetype:xml",
+    "site:dropbox.com inurl:\"coupon\" filetype:txt",
+    "site:drive.google.com \"coupon codes\" filetype:pdf",
+    "site:docs.google.com \"active discounts\" filetype:docx",
+
+    # Social media platforms
+    "site:linkedin.com \"discount promo\"",
+    "site:pinterest.com \"coupon offers\"",
+    "site:youtube.com \"promo code\" intext:\"description\"",
+    "site:tumblr.com inurl:\"coupon\"",
+    "site:quora.com \"active promo code\"",
+    "site:vk.com \"coupon code\"",
+    "site:telegram.me \"promo code\"",
+    "site:discord.com \"discount\"",
+    "site:github.com \"coupon list\"",
+    "site:gitlab.com \"promo codes\"",
+
+    # E-commerce and marketplaces
+    "site:ebay.com \"promo code\"",
+    "site:amazon.com \"coupon\"",
+    "site:walmart.com \"discount\"",
+    "site:bestbuy.com \"promo code\"",
+    "site:target.com \"coupon\"",
+    "site:alibaba.com \"discount codes\"",
+    "site:etsy.com \"coupon offers\"",
+    "site:rakuten.com \"promo code\"",
+    "site:shopify.com \"discount coupons\"",
+
+    # Educational and institutional
+    "site:*.edu \"student discount\"",
+    "site:*.edu intext:\"coupon\"",
+    "site:*.gov \"discount programs\"",
+    "site:*.gov intext:\"active promo codes\"",
+
+    # Generic keyword searches
+    "\"discount codes for online shopping\"",
+    "\"promo code for checkout\"",
+    "\"coupon codes 2024\"",
+    "\"valid promo codes 2024\"",
+    "\"discount offers on electronics\"",
+    "\"active discounts\" site:*.org",
+    "\"exclusive promo codes\"",
+    "\"best discount coupons\"",
+    "\"recently verified promo codes\"",
+    "\"deal of the day promo codes\"",
+    "\"weekly discount offers\"",
+
+    # File hosting and sharing sites
+    "site:mediafire.com inurl:\"coupon\"",
+    "site:4shared.com \"promo code\"",
+    "site:mega.nz \"discount codes\"",
+    "site:onedrive.live.com intext:\"coupon list\"",
+    "site:we.tl \"promo codes\"",
+
+    # Coupon-specific sites
+    "site:slickdeals.net inurl:\"coupon\"",
+    "site:couponfollow.com \"promo code\"",
+    "site:coupons.com inurl:\"discount\"",
+    "site:savings.com \"active promo code\"",
+    "site:couponcabin.com inurl:\"coupons\"",
+    "site:offers.com \"promo code\"",
+    "site:dealsea.com intext:\"discount\"",
+    "site:dealnews.com \"coupon offers\"",
+    "site:promocodes.com \"active coupons\"",
+    "site:groupon.com \"daily deals\"",
+
+    # Expanding keywords and platforms
+    "\"promo code generator\"",
+    "\"exclusive discounts\"",
+    "\"limited time offers\"",
+    "\"online shopping deals\"",
+    "\"flash sale coupon codes\"",
+    "\"clearance sale promo codes\"",
+    "\"early bird discounts\"",
+    "\"top-rated discounts\"",
+    "\"most popular promo codes\"",
+    "\"new user promo codes\"",
+    "\"first order discounts\"",
+    "\"subscription coupon codes\"",
+
+    # Miscellaneous sites
+    "site:tripadvisor.com \"discount offers\"",
+    "site:booking.com \"promo code\"",
+    "site:airbnb.com \"coupon\"",
+    "site:expedia.com \"discounts\"",
+    "site:hotels.com \"promo codes\"",
+    "site:ubereats.com \"coupon offers\"",
+    "site:doordash.com \"discounts\"",
+    "site:grubhub.com \"promo codes\"",
+    "site:netflix.com \"coupon\"",
+    "site:spotify.com \"discount\"",
+    "site:hulu.com \"promo code\"",
     ]
     with open(filename, "w") as file:
         file.write("\n".join(dorks))
